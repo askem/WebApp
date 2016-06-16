2265d8459c37c9e530954f3501ca436dfeb75ee6
Yoni H <yonihemi@gmail.com>
2016-06-16 19:13:16 +0800

    Added style-loader,css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
 				exclude: /node_modules/,
 				loader: 'babel',
 				query: { presets: [ 'es2015', 'react' ] }
-			}
+			},
+			{ test: /\.css$/, loader: "style-loader!css-loader" }
 		]
 	},
 
