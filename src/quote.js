fee957e71fbed4b390608e9d61fe1ef9b10a7cef
Yoni H <yonihemi@gmail.com>
2016-11-22 20:11:50 +0800

    Update Analytics integrations

--- src/quote.js
+++ src/quote.js
@@ -122,19 +122,43 @@ render(
 	document.getElementById('app')
 );
 
+/* Integrations */
 if (__PRODUCTION__) {
-	
+	// Google Analytics
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-34753664-1', 'auto');
+	ga('create', 'UA-87736891-2', 'auto');
 	ga('send', 'pageview');
 	
+	// Facebook Pixel
+	!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '1848903508664772'); 
+	fbq('track', 'PageView');
 	
+	// Intercom
 	window.intercomSettings = {
 	  app_id: "p1iplum8"
 	};
  	(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/p1iplum8';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-		
+	
+	// Linkedin
+	_linkedin_data_partner_id = "21993";
+	(function(){var s = document.getElementsByTagName("script")[0];
+	var b = document.createElement("script");
+	b.type = "text/javascript";b.async = true;
+	b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+	s.parentNode.insertBefore(b, s);})();
+	
+	// Askem Pixel
+	
+	
 }
