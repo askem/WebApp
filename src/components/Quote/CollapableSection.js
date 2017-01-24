1172d3023a83c48cb3c50d6ff7796bba0d545f55
lior <lior.shtivelman@askem.com>
2017-01-24 18:03:07 +0200

    moved section after behavior to advanced panel

---
+++ src/components/Quote/CollapableSection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ToggleArrow from 'components/Quote/ToggleArrow';
+
+
+class CollapableSection extends React.Component {
+	constructor(props){
+		super(props);
+		this.toggleState = this.toggleState.bind(this);
+		this.state = {
+			collapsed : true,
+			titleWidth : 80
+
+		}
+	}
+
+
+	toggleState() {
+		let { collapsed } = this.state;
+
+		this.setState({
+			collapsed:!collapsed
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="quote-audience react-selectize-toggle-button-container collapsable-header" onClick={this.toggleState}>
+						<div style={{ width:this.state.titleWidth + 'px' }}>{ this.props.title }</div>
+						<div>
+							<ToggleArrow collapsed={ this.state.collapsed }/>
+						</div>
+				</div>
+				<div style={{display:this.state.collapsed ? 'none' : 'block'}}>
+						{ this.props.children }
+				</div>
+			</div>
+		)
+	}
+}
+
+export default CollapableSection;
